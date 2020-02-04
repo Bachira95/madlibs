dd1fcfd3299143d23b1036039859abaac18d1bf6
basheera <basheera.k.a@hotmail.com>
2020-02-04 16:49:55 +0300

    madlips

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,14 @@
 def main():
 	# write your code here
+	time = int(input("Enter the time: "))
+	
+	items=input("enter name of items: ")
+	
+	name= input("Enter a name: ")
+	scream = input("Enter the scream: ")
+	action = input ("What action!: ")
+
+	print ("It was %d o'clock when I heard a knock at the door.\nI opened the door and there was a box full of %s with a note saying \" From Mr. %s.\"\nJust as I closed the door I heard a scream\"%s.\"\nI froze in place and all I could do was %s"%(time, items, name.title(), scream.upper(), action))
 
 
 
